181349cc819d163fa73d6fe7d3b5b3146e7343b2
Ashley Harrop <144391801+ashleyharrop@users.noreply.github.com>
2023-09-13 11:33:07 -0400

    1st commit

--- sketch.js
+++ sketch.js
@@ -1,7 +1,20 @@
+let x = 200;
+let speed = 5;
+let diameter = 40;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400,400);
+  
 }
 
 function draw() {
-  background(125);
+  background(0);
+  ellipse(x,height/2,40);
+  if (x >= width - diameter/2 || x <= 0) 
+  {
+  speed = speed*-1;
+  }
+  x += speed;
+
 }
+
